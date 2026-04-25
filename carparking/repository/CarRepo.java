b838df289bb31c63c2d985e00ca3fa7f3f9b6577
Shifana-2006 <shifanaofficial27@gmail.com>
2026-04-25 05:34:56 +0530

    Delete carparking directory

--- carparking/repository/CarRepo.java
+++
@@ -1,8 +0,0 @@
-package com.car.carparking.repository;
-
-import com.car.carparking.dao.Cars;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-    public interface CarRepo extends JpaRepository<Cars,Integer> {
-
-    }
--- carparking/repository/CarRepository.java
+++
@@ -1,4 +0,0 @@
-package com.car.carparking.repository;
-
-public class CarRepository {
-}
